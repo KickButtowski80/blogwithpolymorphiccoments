b7d7e6f4714a6c1228d4a9bf2f8f29816c3eb23f
kickbuttowski59 <pazpaz22@yahoo.com>
2017-11-09 17:25:13 +0000

    striping trailling spaces for article title and text in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,5 +15,19 @@ class Article < ApplicationRecord
     has_many :comments, as: :commentable , :dependent => :destroy  , autosave: true 
     validates :title, presence: true, length:{ minimum: 5}
     # validates_presence_of :comments, on: :body
+    before_validation :strip_whitespace
+     
+     
+     
+  def strip_whitespace      	
+      #I added an unless self.name.blank? behind them, to avoid stripping NIL-values. 
+      self.title = self.title.strip unless self.title.nil? 
+      self.text  = self.text.gsub! /(\A\s*|\s*\z)/, ''  unless self.text.nil?
+        #	I realize this is very old, but I wanted to point out two things..
+        #   First, instead of self.name = self.name.strip unless self.name.nil?,
+        #   I've come to prefer self.name.try(&:strip!). But if you are really
+        #   looking to remove whitespace from the beginning and end,
+        #   I find self.name.gsub! /(\A\s*|\s*\z)/, '' to be most reliable.
+  end   
      
 end
